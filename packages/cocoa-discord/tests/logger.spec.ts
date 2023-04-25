a58dcbe02abcef5e51c48397cf95e417ed782260
Nutthapat Pongtanyavichai <59821765+Leomotors@users.noreply.github.com>
2023-04-25 16:50:22 +0700

    chore: update linting

--- packages/cocoa-discord/tests/logger.spec.ts
+++ packages/cocoa-discord/tests/logger.spec.ts
@@ -13,6 +13,7 @@ describe("[logger] Logger", () => {
   it("Success should be Green", () => {
     const f = Cocoa.format("Hello World", LogStatus.Success);
 
+    // eslint-disable-next-line no-control-regex
     expect(f).toMatch(/^\u001b\[32m/);
   });
 });
